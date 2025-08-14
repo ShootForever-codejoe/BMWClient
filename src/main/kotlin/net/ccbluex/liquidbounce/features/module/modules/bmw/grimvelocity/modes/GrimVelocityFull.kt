26cb109e1d6f2cafa74ce0f17f97b69acb452ae3
ShootForever <2461719257@qq.com>
2025-08-14 20:21:52 +0800

    7.1 grim full small fix

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/bmw/grimvelocity/modes/GrimVelocityFull.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/bmw/grimvelocity/modes/GrimVelocityFull.kt
@@ -9,13 +9,17 @@ import net.ccbluex.liquidbounce.event.events.PlayerTickEvent
 import net.ccbluex.liquidbounce.event.events.TransferOrigin
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.sequenceHandler
+import net.ccbluex.liquidbounce.event.tickHandler
 import net.ccbluex.liquidbounce.features.module.modules.bmw.grimvelocity.ModuleGrimVelocity
 import net.ccbluex.liquidbounce.utils.aiming.RotationManager
 import net.ccbluex.liquidbounce.utils.aiming.data.Rotation
 import net.ccbluex.liquidbounce.utils.aiming.utils.raycast
+import net.ccbluex.liquidbounce.utils.block.getBlock
+import net.ccbluex.liquidbounce.utils.block.isInteractable
 import net.ccbluex.liquidbounce.utils.client.PacketSnapshot
 import net.ccbluex.liquidbounce.utils.client.handlePacket
 import net.ccbluex.liquidbounce.utils.inventory.InventoryManager
+import net.ccbluex.liquidbounce.utils.kotlin.random
 import net.minecraft.client.gui.screen.ingame.GenericContainerScreen
 import net.minecraft.item.consume.UseAction
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket
@@ -27,7 +31,6 @@ import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket
 import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
-import net.minecraft.util.hit.BlockHitResult
 
 object GrimVelocityFull : Choice("Full") {
 
@@ -38,15 +41,11 @@ object GrimVelocityFull : Choice("Full") {
     private val onlyOnGround by boolean("OnlyOnGround", true)
     private val debug by boolean("Debug", false)
 
-    private const val BLOCK_HIT_PITCH = 89.79f
-
     private var canCancel = false
     private var delay = false
     private var needClick = false
     private var waitForUpdate = false
-    private var hitResult: BlockHitResult? = null
     private var shouldSkip = false
-    private var stuckTicks = 0
     private val delayedPacketQueue = Queues.newConcurrentLinkedQueue<PacketSnapshot>()
 
     override fun enable() {
@@ -54,9 +53,7 @@ object GrimVelocityFull : Choice("Full") {
         delay = false
         needClick = false
         waitForUpdate = false
-        hitResult = null
         shouldSkip = false
-        stuckTicks = 0
         delayedPacketQueue.clear()
     }
 
@@ -65,7 +62,7 @@ object GrimVelocityFull : Choice("Full") {
         delayedPacketQueue.clear()
     }
 
-    @Suppress("unused")
+    @Suppress("unused", "DEPRECATION")
     private val packetEventHandler = sequenceHandler<PacketEvent> { event ->
         val packet = event.packet
 
@@ -105,73 +102,87 @@ object GrimVelocityFull : Choice("Full") {
         if (((packet is EntityVelocityUpdateS2CPacket && packet.entityId == player.id)
                 || packet is ExplosionS2CPacket)
             && canCancel
-            && player.activeItem.useAction != UseAction.EAT
-            && player.activeItem.useAction != UseAction.DRINK
-            && !InventoryManager.isInventoryOpen
-            && mc.currentScreen !is GenericContainerScreen
-            && (!onlyOnGround || player.isOnGround)
         ) {
-            event.cancelEvent()
-            delay = true
+            val hitResult = raycast(rotation = Rotation(player.yaw, 90f))
+            val pos = hitResult.blockPos.offset(hitResult.side)
+            val blockState = world.getBlockState(hitResult.blockPos)
+            if (player.activeItem.useAction != UseAction.EAT
+                && player.activeItem.useAction != UseAction.DRINK
+                && !InventoryManager.isInventoryOpen
+                && mc.currentScreen !is GenericContainerScreen
+                && (!onlyOnGround || player.isOnGround)
+                && !hitResult.blockPos.getBlock().isInteractable(blockState)
+                && blockState.isSolid
+                && blockState.isOpaqueFullCube
+            ) {
+                event.cancelEvent()
+                delay = true
+                needClick = true
+                if (debug) notifyAsMessage("[GrimVelocity] Cancel Velocity")
+            } else {
+                if (debug) notifyAsMessage("[GrimVelocity] Receive Velocity")
+            }
             canCancel = false
-            needClick = true
         }
     }
 
     @Suppress("unused")
     private val playerTickEventHandler = handler<PlayerTickEvent> { event ->
         if (needClick) {
-            hitResult = raycast(rotation = Rotation(player.yaw, BLOCK_HIT_PITCH))
-            val pos = hitResult!!.blockPos.offset(hitResult!!.side)
-            if (!pos.equals(player.blockPos) || shouldSkip) {
-                hitResult = null
-            }
-        }
+            val hitResultYaw = player.yaw - (0.01f..0.1f).random()
+            val hitResultPitch = 90f - (0.01f..0.1f).random()
+            val hitResult = raycast(rotation = Rotation(hitResultYaw, hitResultPitch))
+            val pos = hitResult.blockPos.offset(hitResult.side)
 
-        if (hitResult != null) {
-            delay = false
-            delayedPacketQueue.forEach { handlePacket(it.packet) }
-            delayedPacketQueue.clear()
+            if (pos.equals(player.blockPos) && !shouldSkip) {
+                delay = false
+                delayedPacketQueue.forEach { handlePacket(it.packet) }
+                delayedPacketQueue.clear()
 
-            if (interaction.interactBlock(player, Hand.MAIN_HAND, hitResult) == ActionResult.SUCCESS) {
-                player.swingHand(Hand.MAIN_HAND)
-            }
+                if (interaction.interactBlock(player, Hand.MAIN_HAND, hitResult) == ActionResult.SUCCESS) {
+                    player.swingHand(Hand.MAIN_HAND)
+                }
 
-            if (RotationManager.serverRotation.pitch != BLOCK_HIT_PITCH) {
-                network.sendPacket(
-                    PlayerMoveC2SPacket.LookAndOnGround(
-                        player.yaw,
-                        BLOCK_HIT_PITCH,
-                        player.isOnGround,
-                        player.horizontalCollision
+                if (RotationManager.serverRotation.yaw != hitResultYaw || RotationManager.serverRotation.pitch != hitResultPitch) {
+                    network.sendPacket(
+                        PlayerMoveC2SPacket.LookAndOnGround(
+                            hitResultYaw,
+                            hitResultPitch,
+                            player.isOnGround,
+                            player.horizontalCollision
+                        )
                     )
-                )
-            } else {
-                network.sendPacket(
-                    PlayerMoveC2SPacket.OnGroundOnly(
-                        player.isOnGround,
-                        player.horizontalCollision
+                } else {
+                    network.sendPacket(
+                        PlayerMoveC2SPacket.OnGroundOnly(
+                            player.isOnGround,
+                            player.horizontalCollision
+                        )
                     )
-                )
-            }
+                }
 
-            stuckTicks = 0
-            waitForUpdate = true
-            hitResult = null
-            needClick = false
+                waitForUpdate = true
+                needClick = false
+            }
         }
 
         if (waitForUpdate) {
             event.cancelEvent()
-            stuckTicks++
-            if (stuckTicks > maxStuckTicks) {
-                waitForUpdate = false
-                needClick = false
-                if (debug) notifyAsMessage("[IRC] Max Stuck Ticks")
-            }
         }
 
         shouldSkip = false
+    }
+
+    @Suppress("unused")
+    private val tickHandler = tickHandler {
+        waitUntil { waitForUpdate }
+        repeat(maxStuckTicks) {
+            waitTicks(1)
+            if (!waitForUpdate) return@tickHandler
+        }
+        waitForUpdate = false
+        needClick = false
+        if (debug) notifyAsMessage("[GrimVelocity] Max Stuck Ticks")
     }
 
 }
